beed15bf7aedfa3927443d8b166370ef5e5808e4
eunveloper <eune6460@gmail.com>
2024-07-02 22:00:51 +0900

    print 출력 코드 제거 및 로그 패키지로 변경 - 불필요한 출력코드는 제거 - 컴포넌트 생성시 1회 호출되는 데이터베이스 정보 확인용 출력코드는 로그 패키지로 변경

--- src/main/java/antigravity/config/H2Runner.java
+++ src/main/java/antigravity/config/H2Runner.java
@@ -1,32 +1,31 @@
 package antigravity.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
 @Component
+@Slf4j
 public class H2Runner implements ApplicationRunner {
 
     @Autowired
     DataSource dataSource;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
         try (Connection connection = dataSource.getConnection()) {
-            System.out.println(connection);
-            String URL = connection.getMetaData().getURL();
-            System.out.println(URL);
-            String User = connection.getMetaData().getUserName();
-            System.out.println(User);
+            log.info("======================================== database info ========================================");
+            log.info("connection : {}", connection);
+            String url = connection.getMetaData().getURL();
+            log.info("url        : {}", url);
+            String user = connection.getMetaData().getUserName();
+            log.info("user       : {}", user);
+            log.info("===============================================================================================");
         }
     }
 }
--- src/main/java/antigravity/service/ProductService.java
+++ src/main/java/antigravity/service/ProductService.java
@@ -13,8 +13,6 @@ public class ProductService {
     private final ProductRepository repository;
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request) {
-        System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
-
         Product product = repository.getProduct(request.getProductId());
 
         return null;
